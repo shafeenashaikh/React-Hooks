6ca2c08271eb498fa7625dc042a4938c34b72be1
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-11 12:33:06 +0530

    Post Formdata to the server with axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import PostForm from './component/PostForm';
 import PostList from './component/PostList';
 
 function App() {
   return (
     <div className="App">
-       <PostList/>
+       {/* <PostList/> */}
+       <PostForm/>
     </div>
   );
 }
