e6b39a7eeedac376aa7b06afd29e3ce9180b654d
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-11 14:40:15 +0530

    counter in functional component using React Hooks(usestate hook)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import PostForm from './component/PostForm';
 import PostList from './component/PostList';
 import ClassCount from './Hooks/ClassCount';
+import HookCounter from './Hooks/HookCounter';
 
 function App() {
   return (
     <div className="App">
        {/* <PostList/> */}
        {/* <PostForm/> */}
-       <ClassCount/>
+       {/* <ClassCount/> */}
+       <HookCounter/>
     </div>
   );
 }
