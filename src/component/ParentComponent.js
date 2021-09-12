033c96f5abb1dca62fa9b4ba24bd44342276261e
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-12 16:51:10 +0530

    Rerendring neccessary called value using usecallback hook

--- src/component/ParentComponent.js
+++ src/component/ParentComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Title from "./Title";
 import Count from './Count'
 import Button from './Button'
@@ -7,13 +7,13 @@ function ParentComponent(){
     const [age, setAge] = useState(25)
     const [salary, setSalary] = useState(50000)
 
-    const incrementAge = () => {
+    const incrementAge = useCallback(() => {
         setAge(age + 1)
-    }
+    }, [age]) 
 
-    const incrementSalary = () => {
+    const incrementSalary = useCallback(() => {
         setSalary(salary + 1000)
-    }
+    },[salary])
     return(
         <div>
             <Title/>
